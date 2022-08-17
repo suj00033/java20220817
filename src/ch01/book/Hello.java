00b94fc689245804a4753ba631ccf2b7037423fb
suj00033 <88014400+suj00033@users.noreply.github.com>
2022-08-17 12:13:36 +0900

    first commit

---
+++ src/ch01/book/Hello.java
@@ -0,0 +1,12 @@
+package ch01.book;
+/*
+ * 작성자: 신용권
+ * 작성일: 2015.12.31
+ */
+public class Hello {
+	//프로그램 실행 진입점
+	public static void main(String[] args) {
+		//콘솔에 출력하는 실행문
+		System.out.println("Hello, welcome th the java world!");
+	} //end of main
+}	//end of class
---
+++ src/ch01/lecture/Comment.java
@@ -0,0 +1,26 @@
+package ch01.lecture;
+
+public class Comment {
+	public static void main(String[] args) {
+		// 주석 : 프로그램 실행에 영향을 미치지 않음
+		// 한줄 주석
+		
+		/*
+		 * 여러줄 주석
+		 * 여러줄 주석
+		 */
+		
+		// 단축키 : ctrl + /
+		
+		// 힌트 얻기 : ctrl + space
+		// 한줄 지우기 : ctrl + d
+		// 코드 위아래로 옮기기 : alt + 화살표 위아래		
+		
+		// 콘솔 출력 코드 : sysout / sout
+		
+		System.out.println("Hello world");
+		System.out.println("자바는 쉽지 않다");
+		System.out.println("잘 배워서 써먹어 보자");
+		System.out.println("콘솔에 출력");
+	}
+}
