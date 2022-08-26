b23305653ed2a237170b3941a3e57099cf73d3fd
suj00033 <88014400+suj00033@users.noreply.github.com>
2022-08-26 16:57:10 +0900

    Create WarmUp1NotString.java

---
+++ src/p99codingbat/WarmUp1NotString.java
@@ -0,0 +1,11 @@
+package p99codingbat;
+
+public class WarmUp1NotString {
+	public String notString(String str) {
+		if (str.length() >= 3 && str.substring(0, 3).equals("not")) {
+			return str;
+		} else {
+			return "not " + str;
+		}
+	}
+}
