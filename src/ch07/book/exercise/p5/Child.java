e9fa2efac46a27972179f788ac2965e0501df03e
suj00033 <suj00033@naver.com>
2022-08-31 16:49:03 +0900

    0831

---
+++ src/ch07/book/exercise/p5/Child.java
@@ -0,0 +1,11 @@
+package ch07.book.exercise.p5;
+
+public class Child extends Parent {
+	
+	private int studentNo;
+	
+	public Child(String name, int studentNo) {
+		super(name);
+		this.studentNo = studentNo;
+	}
+}
